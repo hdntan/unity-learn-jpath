52d5ca34c86b0fd5469883fdad3594679cabd3c1
hdntan <hdntan0903@gmail.com>
2025-07-05 16:09:33 +0700

    feat: spawn random animal

--- Assets/Scripts/P2/SpawnManager.cs
+++ Assets/Scripts/P2/SpawnManager.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+
+    public int animalIndex;
+
+    public float spawnRangeX = 20f;
+    public float spawnPosZ = 20f;
+
     void Start()
     {
         
@@ -11,6 +17,12 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-this.spawnRangeX, this.spawnRangeX), 0, this.spawnPosZ);
+             this.animalIndex = Random.Range(0, this.animalPrefabs.Length);
+            Instantiate(this.animalPrefabs[this.animalIndex], spawnPos, this.animalPrefabs[this.animalIndex].transform.rotation);
+        }
     }
 }
